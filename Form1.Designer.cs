295a80ac3e56a5f46461de2a9f72917e531a9b65
dmitry2k <dmitry.lyudovskikh@gmail.com>
2016-06-25 15:22:30 +0300

    release version

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Fractal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1235, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(98, 20);
+            this.openToolStripMenuItem.Text = "&Открыть файл";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "txt";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "TxtFiles|*.txt";
+            this.openFileDialog1.Title = "Открытие файла";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1211, 703);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(134, 33);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Количество итераций:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(650, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Нарисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(315, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Длина элемента:";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(416, 35);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 6;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1235, 776);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Fractal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Dictionary<char, string> rules;
+        public string generic_string;
+        public int length;
+        public double angle, direction;
+
+        public class MyPoint
+        {
+            private double x, y;
+            private Color c;
+
+            public double X
+            {
+                get
+                {
+                    return x;
+                }
+            }
+
+            public double Y
+            {
+                get
+                {
+                    return y;
+                }
+            }
+
+            public Color Color
+            {
+                get
+                {
+                    return c;
+                }
+            }
+
+            public MyPoint(double x, double y, Color c)
+            {
+                this.x = x;
+                this.y = y;
+                this.c = c;
+            }
+
+            public MyPoint(double x, double y)
+            {
+                this.x = x;
+                this.y = y;
+                this.c = Color.Black;
+            }
+
+            public void RotationPoint(MyPoint p, double rotation)
+            {
+                double new_x = (x - p.X) * Math.Cos(rotation) - (y - p.Y) * Math.Sin(rotation) + p.X;
+                double new_y = (x - p.X) * Math.Sin(rotation) + (y - p.Y) * Math.Cos(rotation) + p.Y;
+                x = new_x;
+                y = new_y;
+            }
+
+            public Bitmap DrawPoint(Bitmap bm)
+            {
+                if (this.x > 0 && this.x < bm.Width && this.y > 0 && this.y < bm.Height)
+                    bm.SetPixel((int)this.x, (int)this.y, this.c);
+                return bm;
+            }
+
+            public Bitmap DrawBigPoint(Bitmap bm)
+            {
+                if (this.x > 1 && this.x < bm.Width - 1 && this.y > 1 && this.y < bm.Height - 1)
+                    for (int i = -1; i < 2; ++i)
+                        for (int j = -1; j < 2; ++j)
+                            bm.SetPixel((int)this.x + i, (int)this.y + j, this.c);
+                else
+                    bm = this.DrawPoint(bm);
+                return bm;
+            }
+
+        }
+        public class MyEdge
+        {
+            private MyPoint p1, p2;
+
+            public MyPoint P1
+            {
+                get { return p1; }
+            }
+
+            public MyPoint P2
+            {
+                get { return p2; }
+            }
+
+            public MyEdge(MyPoint p1, MyPoint p2)
+            {
+                this.p1 = p1;
+                this.p2 = p2;
+            }
+
+            public void RotationEdge(MyPoint p, double rotation)
+            {
+                p1.RotationPoint(p, rotation);
+                p2.RotationPoint(p, rotation);
+            }
+
+            public Bitmap DrawEdge(Bitmap bm)
+            {
+                int x1 = (int)p1.X;
+                int y1 = (int)p1.Y;
+                int x2 = (int)p2.X;
+                int y2 = (int)p2.Y;
+                int dx = Math.Abs(x2 - x1);
+                int dy = Math.Abs(y2 - y1);
+                int sx, sy;
+                if (x2 >= x1)
+                    sx = 1;
+                else
+                    sx = -1;
+                if (y2 >= y1)
+                    sy = 1;
+                else
+                    sy = -1;
+                if (dy < dx)
+                {
+                    int d = 2 * dy - dx;
+                    int d1 = 2 * dy;
+                    int d2 = 2 * (dy - dx);
+                    bm.SetPixel(x1, y1, p1.Color);
+                    int x = x1 + sx;
+                    int y = y1;
+                    for (int i = 1; i <= dx; i++)
+                    {
+                        if (d > 0)
+                        {
+                            d += d2;
+                            y += sy;
+                        }
+                        else
+                            d += d1;
+                        bm.SetPixel(x, y, p1.Color);
+                        x += sx;
+                    }
+                }
+                else
+                {
+                    int d = 2 * dx - dy;
+                    int d1 = 2 * dx;
+                    int d2 = 2 * (dx - dy);
+                    bm.SetPixel(x1, y1, p1.Color);
+                    int x = x1;
+                    int y = y1 + sy;
+                    for (int i = 1; i <= dy; i++)
+                    {
+                        if (d > 0)
+                        {
+                            d += d2;
+                            x += sx;
+                        }
+                        else
+                            d += d1;
+                        bm.SetPixel(x, y, p1.Color);
+                        y += sy;
+                    }
+                }
+                return bm;
+            }
+
+            static public Bitmap DrawEdge(Bitmap bm, MyPoint p1, MyPoint p2)
+            {
+                int x1 = (int)p1.X;
+                int y1 = (int)p1.Y;
+                int x2 = (int)p2.X;
+                int y2 = (int)p2.Y;
+                int dx = Math.Abs(x2 - x1);
+                int dy = Math.Abs(y2 - y1);
+                int sx, sy;
+                if (x2 >= x1)
+                    sx = 1;
+                else
+                    sx = -1;
+                if (y2 >= y1)
+                    sy = 1;
+                else
+                    sy = -1;
+                if (dy < dx)
+                {
+                    int d = 2 * dy - dx;
+                    int d1 = 2 * dy;
+                    int d2 = 2 * (dy - dx);
+                    bm.SetPixel(x1, y1, p1.Color);
+                    int x = x1 + sx;
+                    int y = y1;
+                    for (int i = 1; i <= dx; i++)
+                    {
+                        if (d > 0)
+                        {
+                            d += d2;
+                            y += sy;
+                        }
+                        else
+                            d += d1;
+                        bm.SetPixel(x, y, p1.Color);
+                        x += sx;
+                    }
+                }
+                else
+                {
+                    int d = 2 * dx - dy;
+                    int d1 = 2 * dx;
+                    int d2 = 2 * (dx - dy);
+                    bm.SetPixel(x1, y1, p1.Color);
+                    int x = x1;
+                    int y = y1 + sy;
+                    for (int i = 1; i <= dy; i++)
+                    {
+                        if (d > 0)
+                        {
+                            d += d2;
+                            x += sx;
+                        }
+                        else
+                            d += d1;
+                        bm.SetPixel(x, y, p1.Color);
+                        y += sy;
+                    }
+                }
+                return bm;
+            }
+
+            public int WherePoint(MyPoint p)
+            {
+                double s = (p2.X - p1.X) * (p.Y - p1.Y) - (p2.Y - p1.Y) * (p.X - p1.X);
+                if (s > 0)
+                    return 1;
+                else if (s < 0)
+                    return -1;
+                else
+                    return 0;
+            }
+
+            public MyPoint IntersectionEdge(MyEdge edge)
+            {
+                double k1 = (p2.X - p1.X) / (p2.Y - p1.Y);
+                double k2 = (edge.P2.X - edge.P1.X) / (edge.P2.Y - edge.P1.Y);
+                if (k1 == k2)
+                    return null;
+                double x = ((p1.X * p2.Y - p2.X * p1.Y) * (edge.P2.X - edge.P1.X) - (edge.P1.X * edge.P2.Y - edge.P2.X * edge.P1.Y) * (p2.X - p1.X)) / ((p1.Y - p2.Y) * (edge.P2.X - edge.P1.X) - (edge.P1.Y - edge.P2.Y) * (p2.X - p1.X));
+                double y = ((edge.P1.Y - edge.P2.Y) * x - (edge.P1.X * edge.P2.Y - edge.P2.X * edge.P1.Y)) / (edge.P2.X - edge.P1.X);
+                if (x >= Math.Min(Math.Min(p1.X, p2.X), Math.Min(edge.P1.X, edge.P2.X)) && x <= Math.Max(Math.Max(p1.X, p2.X), Math.Max(edge.P1.X, edge.P2.X)) && y >= Math.Min(Math.Min(p1.Y, p2.Y), Math.Min(edge.P1.Y, edge.P2.Y)) && y <= Math.Max(Math.Max(p1.Y, p2.Y), Math.Max(edge.P1.Y, edge.P2.Y)))
+                    return new MyPoint(x, y);
+                else
+                    return null;
+            }
+        }
+
+        public string CreateFractal(Dictionary<char, string> rules, string generic_string, int count_iterations)
+        {
+            string result = generic_string;
+            for (int i = 0; i < count_iterations; ++i)
+            {
+                int l = result.Length - 1;
+                for (int j = l; j >= 0; --j)
+                    if (rules.ContainsKey(result[j]))
+                    {
+                        result=result.Insert(j+1,rules[result[j]]);
+                        result=result.Remove(j, 1);
+                    }
+            }
+            return result;
+        }
+
+        //+(-) - поворот по (против) часовой стрелке на угол
+        //F,G,D - рисуют прямую
+        public Bitmap DrawFractal(string fractal, double direction, double angle, int length, Bitmap bm)
+        {
+            if (fractal==null)
+                return bm;
+            MyPoint p1 = new MyPoint(bm.Width/2, bm.Height / 2);
+            MyPoint p2 = new MyPoint(bm.Width/2, bm.Height / 2 - length);
+            Stack<KeyValuePair<MyPoint, double>> s = new Stack<KeyValuePair<MyPoint, double>>();
+            for (int i = 0; i < fractal.Length; ++i)
+            {
+                if (fractal[i] == '+')
+                    direction += angle;
+                if (fractal[i] == '-')
+                    direction -= angle;
+                if (fractal[i] == 'F' || fractal[i] == 'G' || fractal[i] == 'D')
+                {
+                    p2.RotationPoint(p1, direction);
+                    if (0<p1.X && 0<p1.Y && 0<p2.X && 0<p2.Y &&  bm.Width>p1.X && bm.Height>p1.Y && bm.Width>p2.X && bm.Height>p2.Y)
+                        bm = MyEdge.DrawEdge(bm, p1, p2);
+                    p1 = p2;
+                    p2 = new MyPoint(p1.X, p1.Y - length);
+                }
+                if (fractal[i] == '[')
+                    s.Push(new KeyValuePair<MyPoint, double>(p1, direction));
+                if (fractal[i] == ']')
+                {
+                    p1 = s.Peek().Key;
+                    direction = s.Pop().Value;
+                    p2 = new MyPoint(p1.X, p1.Y - length);
+                }
+            }
+            return bm;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rules = new Dictionary<char, string>();
+            openFileDialog1.FileName = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                string fn = openFileDialog1.FileName;
+                FileInfo fi = new FileInfo(fn);
+                StreamReader sr = new StreamReader(fn);
+                generic_string = sr.ReadLine();
+                string read = null;
+                while ((read = sr.ReadLine()) != null)
+                {
+                    if (read.Length>1 && read[1] == '-')
+                        rules.Add(read[0], read.Substring(3));
+                    else
+                    {
+                        direction = Double.Parse(read) * Math.PI / 180;
+                        angle = Double.Parse(sr.ReadLine()) * Math.PI / 180;
+                        read = sr.ReadLine();
+                        if (read != null)
+                        {
+                            length = Int32.Parse(read);
+                            numericUpDown2.Value = length;
+                        }
+                    }
+                }
+                sr.Close();
+            }
+            numericUpDown1.Value = 0;
+            button1_Click(sender, e);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (rules == null)
+                return;
+            Bitmap bm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            bm = DrawFractal(CreateFractal(rules, generic_string, Int32.Parse(numericUpDown1.Value.ToString())), direction, angle, length, bm);
+            pictureBox1.Image = bm;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            if (rules == null)
+                return;
+            length = Int32.Parse(numericUpDown2.Value.ToString());
+            button1_Click(sender, e);
+        }
+    }
+}
